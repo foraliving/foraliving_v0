4b759d25cfee07bf22e878a81b124f0180e51070
metjeanjunior <metjeanjunior@gmail.com>
2016-07-14 20:26:45 -0400

    init admin.py commit

--- foraliving/admin.py
+++ foraliving/admin.py
@@ -1,2 +1,5 @@
 from django.contrib import admin
+from models import * 
 
+modList = [LMS, LMS_Web_Service, School, My_User, Class, Assignment, Question, Answer, Video, Video_Comment, Interview]
+admin.site.register(modList)
