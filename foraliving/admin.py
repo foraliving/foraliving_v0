822d7ab58e555ed6ad366815fec0f03629ca6ce0
metjeanjunior <metjeanjunior@gmail.com>
2016-07-30 17:07:32 -0400

    updated models

--- foraliving/admin.py
+++ foraliving/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import * 
 
 # This should do for the time being. We'll add customization as needed 
-modList = [LMS, LMS_Web_Service, School, My_User, Class, Assignment, Question, Answer, Video, Video_Comment, Interview]
+modList = [LMS, LMS_Web_Service, School, User_Add_Ons, Class, Assignment, Question, Answer, Video, Video_Comment, Interview]
 admin.site.register(modList)
